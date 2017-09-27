e1c941ef96d33a9c8cd31fa1417ff13382c1fc32
Pedro de Assis <pedro@caiena.net>
2017-09-27 10:12:25 -0300

    fix: docker-cmd.sh

--- bin/docker-cmd.sh
+++ bin/docker-cmd.sh
@@ -4,20 +4,16 @@ set -x
 
 NODE_ENV="${NODE_ENV:-development}"
 SCRIPT_HOME=`dirname $0`
+PROJECT_HOME="${SCRIPT_HOME}/.."
 
-if [ $NODE_ENV == "development" ]; then
-  # this runs webpack-dev-server with hot reloading
-  yarn start
-else
-  # build the app
-  yarn run build
+# build the app
+yarn run build
 
-  # copy the dist to nginx html dir
-  cp -R "${SCRIPT_HOME}/dist/*" /usr/share/nginx/html/
+# copy the dist to nginx html dir
+cp -R "${PROJECT_HOME}/dist/*" /usr/share/nginx/html/
 
-  # update nginx default.conf
-  cp "${SCRIPT_HOME}/config/nginx.default.conf" /etc/nginx/conf.d/default.conf
+# update nginx default.conf
+cp "${PROJECT_HOME}/config/nginx.default.conf" /etc/nginx/conf.d/default.conf
 
-  # start nginx
-  nginx -g 'daemon off;'
-fi
+# start nginx
+nginx -g 'daemon off;'
